a4a73fda0b37f572940d06af6a6c1a6f4a62065b
Yiğit ATAMAN <yigitataman1@gmail.com>
2025-04-03 15:53:26 +0300

    Bölüm 6

---
+++ MyPortfolioUdemy/ViewComponents/LayoutViewComponents/_LayoutScriptComponentPartial.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyPortfolioUdemy.DAL.Context;
+
+namespace MyPortfolioUdemy.ViewComponents.LayoutViewComponents
+{
+    public class _LayoutScriptComponentPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
---
+++ MyPortfolioUdemy/ViewComponents/LayoutViewComponents/_LayoutSidebarComponentPartial.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyPortfolioUdemy.DAL.Context;
+
+namespace MyPortfolioUdemy.ViewComponents.LayoutViewComponents
+{
+    public class _LayoutSidebarComponentPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
